4a9f6956487a558ee49dd37159403e3eed279d32
sruthy-j-mallya <sruthyjmallya2018@gmail.com>
2026-02-24 23:20:58 +0530

    Set up react project using vite and load it in electron

---
+++ src/electron/main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  if (app.isPackaged) {
+    mainWindow.loadFile("../../dist/index.html")
+  } else {
+    mainWindow.loadURL("http://localhost:5173/")
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on("window-all-closed", () => {
+  if (process.platform !== 'darwin') app.quit()
+})
