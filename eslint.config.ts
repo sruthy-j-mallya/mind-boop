954765854ac69e02ce520b966e5058788392615c
sruthy-j-mallya <sruthyjmallya2018@gmail.com>
2026-04-08 18:52:09 +0530

    Set up commonly required ESLint rules

---
+++ eslint.config.ts
@@ -0,0 +1,58 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import reactRefresh from "eslint-plugin-react-refresh";
+import { defineConfig } from "eslint/config";
+import unusedImports from "eslint-plugin-unused-imports";
+import reactHooks from "eslint-plugin-react-hooks";
+
+export default defineConfig([
+  {
+    ignores: [
+      "node_modules",
+      "src-tauri",
+      ".cursor",
+      ".vscode",
+      ".public"],
+  },
+  {
+    settings: {
+      react: {
+        version: "19.1.0",
+      },
+    },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    plugins: {
+      js,
+      unusedImports,
+    },
+    extends: [
+      js.configs.recommended,
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      globals: globals.browser
+    }
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  reactHooks.configs.flat["recommended-latest"],
+  {
+    files: ["**/*.{tsx,ts}"],
+    rules: {
+      "no-duplicate-imports": "error",
+      "react/button-has-type": "error",
+      "react/boolean-prop-naming": "warn",
+      "arrow-body-style": ["error", "as-needed"],
+      "func-style": ["error", "expression", { allowArrowFunctions: true }],
+      "react/function-component-definition": ["error", {
+        "namedComponents": "function-expression",
+        "unnamedComponents": "arrow-function"
+      }],
+      "react/react-in-jsx-scope": "off",
+    }
+  }
+]);
