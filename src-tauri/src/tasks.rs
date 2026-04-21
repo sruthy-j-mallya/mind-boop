0a99500cbda30b89141eac5f654953d26493a440
sruthy-j-mallya <sruthyjmallya2018@gmail.com>
2026-04-21 13:21:41 +0530

    Removed unnecessary white spaces

--- src-tauri/src/tasks.rs
+++ src-tauri/src/tasks.rs
@@ -11,7 +11,6 @@ pub async fn create_task(title: String, description: String) -> Result<String, S
         (id.clone(), title, description),
     ).map_err(|e| e.to_string());
 
-
     if result.is_err() {
         return Err(result.err().unwrap().to_string());
     }
@@ -26,8 +25,6 @@ pub async fn update_task(id: String, title: String, description: String) -> Resu
         "UPDATE tasks SET title = ?1, description = ?2 WHERE id = ?3",
         (title, description, id),
     ).map_err(|e| e.to_string());
-
-    
 
     if result.is_err() {
         return Err(result.err().unwrap().to_string());
