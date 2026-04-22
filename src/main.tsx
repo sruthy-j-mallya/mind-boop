1d59d93eca1f9142261333eccb51ed562daf2ea8
sruthy-j-mallya <sruthyjmallya2018@gmail.com>
2026-04-22 19:38:47 +0530

    Mounted Home component in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import App from "./App";
+import Home from "./Home";
 import "./styles/index.css";
 import "./styles/tiptap.css";
 import { TooltipProvider } from "@/components/ui/Tooltip";
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <App />
+        <Home />
       </TooltipProvider>
     </QueryClientProvider>
   </React.StrictMode>,
