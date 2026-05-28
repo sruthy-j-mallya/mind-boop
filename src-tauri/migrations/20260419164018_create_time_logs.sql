96e8554a598b6cf88368385fdf6fac6347738dcc
sruthy-j-mallya <sruthyjmallya2018@gmail.com>
2026-05-28 22:25:01 +0530

    Added timestamps columns in time logs

--- src-tauri/migrations/20260419164018_create_time_logs.sql
+++ src-tauri/migrations/20260419164018_create_time_logs.sql
@@ -3,5 +3,8 @@ CREATE TABLE IF NOT EXISTS time_logs (
     task_id TEXT NOT NULL,
     start_time DATETIME NOT NULL,
     end_time DATETIME NOT NULL,
-    duration INTEGER NOT NULL
+    duration INTEGER NOT NULL,
+    created_at          TEXT    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at          TEXT    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    deleted_at          TEXT
 );
