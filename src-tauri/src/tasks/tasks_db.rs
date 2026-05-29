c0b7fec3b1b4524da92ff9f2babbbceaa7b2966e
sruthy-j-mallya <sruthyjmallya2018@gmail.com>
2026-05-29 20:52:38 +0530

    Added test for tasks listing

--- src-tauri/src/tasks/tasks_db.rs
+++ src-tauri/src/tasks/tasks_db.rs
@@ -22,7 +22,7 @@ pub struct Task {
 pub fn list_db_tasks(search_string: &str, connection: &Connection,) -> Result<Vec<Task>, String> {
   let pattern = format!("%{}%", search_string);
     let mut stmt = connection
-        .prepare("SELECT id, title, description, estimated_minutes, is_duration, is_all_day, is_completed, starts_on, starts_at, ends_on, ends_at FROM tasks WHERE is_completed = 0 AND (title LIKE ?1 OR description LIKE ?1) ORDER BY starts_on IS NULL, starts_on DESC, starts_at IS NULL, starts_at DESC")
+        .prepare("SELECT id, title, description, estimated_minutes, is_duration, is_all_day, is_completed, starts_on, starts_at, ends_on, ends_at FROM tasks WHERE is_completed = 0 AND (title LIKE ?1 OR description LIKE ?1) ORDER BY COALESCE(starts_on, '01-01-3000') DESC, COALESCE(starts_at, '23:59') ASC")
         .map_err(|e| e.to_string())?;
 
     let tasks = stmt
--- src-tauri/tests/tasks_test.rs
+++ src-tauri/tests/tasks_test.rs
@@ -1,12 +1,11 @@
-use mind_boop_lib::tasks::task::list_db_tasks;
+use mind_boop_lib::tasks::tasks_db::{list_db_tasks};
 use rusqlite::Connection;
 use uuid::Uuid;
 
-fn establish_db_connection() -> Connection {
-  Connection::open_in_memory().expect("failed to open in-memory database")
-}
 
-fn create_task_table(connection: &Connection) {
+fn set_up_db() -> Connection {
+  let connection = Connection::open_in_memory().expect("failed to open in-memory database");
+
   connection.execute_batch(
         "CREATE TABLE IF NOT EXISTS tasks (
             id                  TEXT    PRIMARY KEY NOT NULL,
@@ -26,12 +25,13 @@ fn create_task_table(connection: &Connection) {
         );",
     )
     .expect("failed to run migrations");
+
+  connection
 }
 
 #[test]
-pub fn it_lists_tasks_with_a_given_search_string() {
-  let connection = establish_db_connection();
-  create_task_table(&connection);
+pub fn it_lists_uncompleted_tasks_tasks_with_a_given_search_string() {
+  let connection = set_up_db();
 
   let tasks = vec![
     (Uuid::new_v4().to_string(), "Buy groceries", 1),
@@ -40,7 +40,7 @@ pub fn it_lists_tasks_with_a_given_search_string() {
     (Uuid::new_v4().to_string(), "Write integration tests", 0)
   ];
 
-  for (id, title, is_completed) in tasks {
+  for (id, title, is_completed) in &tasks {
     connection.execute(
       "INSERT INTO tasks (id, title, is_completed, created_at, updated_at)
         VALUES (?1, ?2, ?3, datetime('now'), datetime('now'))",
@@ -48,6 +48,81 @@ pub fn it_lists_tasks_with_a_given_search_string() {
     ).expect("failed to insert task");
   };
 
-  let result = list_db_tasks(&connection, "tests");
+  let result = list_db_tasks("tests", &connection);
+
   assert!(result.is_ok());
+
+  let result_tasks = result.expect("Tasks should be present");
+  assert_eq!(result_tasks.len(), 1);
+  if let Some(matched_task) = result_tasks.get(0) {
+    assert_eq!(matched_task.id, tasks[3].0);
+    assert_eq!(matched_task.title, "Write integration tests");
+  }
+}
+
+#[test]
+pub fn it_lists_all_uncompleted_tasks_when_search_string_is_absent() {
+  let connection = set_up_db();
+
+  let tasks = vec![
+    (Uuid::new_v4().to_string(), "Buy groceries", 1),
+    (Uuid::new_v4().to_string(), "Write unit tests", 1),
+    (Uuid::new_v4().to_string(), "Organize study table", 0),
+    (Uuid::new_v4().to_string(), "Write integration tests", 0)
+  ];
+
+  for (id, title, is_completed) in &tasks {
+    connection.execute(
+      "INSERT INTO tasks (id, title, is_completed, created_at, updated_at)
+        VALUES (?1, ?2, ?3, datetime('now'), datetime('now'))",
+      rusqlite::params![id, title, is_completed],
+    ).expect("failed to insert task");
+  };
+
+  let result = list_db_tasks("", &connection);
+
+  assert!(result.is_ok());
+
+  let result_tasks = result.expect("Tasks should be present");
+  assert_eq!(result_tasks.len(), 2);
+
+  let expected_task_ids: Vec<String> = tasks.iter().skip(2).map(|task| task.0.clone()).collect() ;
+  let listed_task_ids: Vec<String> = result_tasks.iter().map(|task| task.id.clone()).collect();
+
+  assert!(expected_task_ids.eq(&listed_task_ids));
+}
+
+#[test]
+#[ignore = "This test will be enabled once all the date and time values are stored as ISO string in SQLite"]
+pub fn it_lists_uncompleted_tasks_in_order_of_starting_date_and_time() {
+  let connection = set_up_db();
+
+  let tasks = vec![
+    (Uuid::new_v4().to_string(), "Buy groceries", Some("01-01-2000"), Some("10:00")),
+    (Uuid::new_v4().to_string(), "Write unit tests", Some("01-01-2000"), None),
+    (Uuid::new_v4().to_string(), "Write integration tests", Some("02-01-2000"), Some("08:00")),
+    (Uuid::new_v4().to_string(), "Organize study table", None, None),
+  ];
+
+  for (id, title, starts_at, starts_on) in &tasks {
+    connection.execute(
+      "INSERT INTO tasks (id, title, starts_at, starts_on, created_at, updated_at)
+        VALUES (?1, ?2, ?3, ?4, datetime('now'), datetime('now'))",
+      rusqlite::params![id, title, starts_at, starts_on],
+    ).expect("failed to insert task");
+  };
+
+  let result = list_db_tasks("", &connection);
+
+  assert!(result.is_ok());
+
+  let result_tasks = result.expect("Tasks should be present");
+  assert_eq!(result_tasks.len(), 4);
+
+  let expected_task_ids: Vec<String> = tasks.iter().map(|task| task.1.to_string()).collect() ;
+  let listed_task_ids: Vec<String> = result_tasks.iter().map(|task| task.title.clone()).collect();
+
+  println!("{:?}", expected_task_ids);
+  println!("{:?}", listed_task_ids);
+  assert!(expected_task_ids.eq(&listed_task_ids));
 }
